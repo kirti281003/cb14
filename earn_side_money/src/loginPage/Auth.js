4ea91e1aefb6260eac2c30ee8785b0bc1a00a9e5
Kirti Aggarwal <kirti057btcse21@igdtuw.ac.in>
2022-11-09 07:52:48 -0800

    database connected

--- earn_side_money/src/loginPage/Auth.js
+++ earn_side_money/src/loginPage/Auth.js
@@ -1,13 +1,96 @@
-import React, { useState } from "react"
+import React, { useState ,useEffect} from "react"
+
 import GoogleLogin from "react-google-login"
 import pic1 from "../images/BlueVector1.png"
 import pic2 from "../images/BlueVector2.png"
+import Axios from "axios";
 
 
 export default function (props) {
-  let [authMode, setAuthMode] = useState("signin")
 
-  const changeAuthMode = () => {
+  let [authMode, setAuthMode] = useState("signin");
+  const[user,setUser]=useState({
+    name:"",email:"",password:""
+  });
+  let name,value;
+  const handleinput=(e)=>{
+    e.preventDefault();
+    name=e.target.name;
+    value=e.target.value;
+    setUser({...user,[name]:value});
+    console.log(e);
+  }
+  const postData=async(e)=>{
+    e.preventDefault();
+    const{name,email,password}=user;
+   const res=await fetch("/signup",{
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json"
+    },
+    body:JSON.stringify({
+      name:name,email:email,password:password
+    })
+
+   });
+   const response=await res.json();
+   if(response.status===422 )
+   {
+    window.alert("Inavlid");
+    console.log("Invalid");
+   }
+   else{
+    window.alert("Success");
+    console.log("Success");
+
+
+   }
+
+  }
+  const[loginuser,setlogin]=useState(
+    {
+      email:"",password:""
+    }
+  );
+  let loginName,loginValue;
+  const handleLogin=(e)=>
+  {e.preventDefault();
+    loginName=e.target.name;
+    loginValue=e.target.value;
+    setlogin({...loginuser,[loginName]:loginValue})
+    console.log(e);
+
+  }
+  const postLogin=async(e)=>{
+    e.preventDefault();
+    const{email,password}=loginuser;
+   const res=await fetch("/signin",{
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json"
+    },
+    body:JSON.stringify({
+      email:email,password:password
+    })
+
+   });
+   const response=await res.json();
+   if(response.status===422 )
+   {
+    window.alert("Inavlid");
+    console.log("Invalid");
+   }
+   else{
+    window.alert("Success");
+    console.log("Success");
+
+
+   }
+
+  }
+ 
+
+ const changeAuthMode = () => {
     setAuthMode(authMode === "signin" ? "signup" : "signin")
   }
 
@@ -23,7 +106,7 @@ export default function (props) {
               buttonText="SIGN IN WITH GOOGLE"
               /*onSuccess={handleLogin}
               onFailure={handleFailure}*/
-              >
+             >
               </GoogleLogin>
             </div>
             <div className="text">
@@ -35,6 +118,9 @@ export default function (props) {
                 type="email"
                 className="form-control mt-1"
                 placeholder="Enter email"
+                name="email"
+                value={loginuser.email}
+                onChange={handleLogin}
               />
             </div>
             <div className="form-group mt-3">
@@ -43,10 +129,13 @@ export default function (props) {
                 type="password"
                 className="form-control mt-1"
                 placeholder="Enter password"
+                name="password"
+                value={loginuser.password}
+                onChange={handleLogin}
               />
             </div>
             <div className="login-button d-grid gap-2 mt-3 mb-3">
-              <button type="button" class="btn btn-primary active" data-bs-toggle="button" aria-pressed="true">
+              <button type="button" class="btn btn-primary active" data-bs-toggle="button" aria-pressed="true" onClick={postLogin}>
                 LOGIN
                 </button>
             </div>
@@ -64,20 +153,22 @@ export default function (props) {
     )
   }
 
+
   return (<>
     <div className="Auth-form-container">
-      <form className="Auth-form">
+      <form className="Auth-form" method="POST">
         <div className="Auth-form-content">
           <h3 className="Auth-form-title">Sign Up</h3>
           <div className="google-login">
               <GoogleLogin
               clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-              buttonText="SIGN UP WITH GOOGLE"
+              buttonText="SIGN IN WITH GOOGLE"
               /*onSuccess={handleLogin}
               onFailure={handleFailure}*/
-              >
+             >
               </GoogleLogin>
             </div>
+          
             <div className="text">
               Or sign up with email ———————
             </div>
@@ -85,36 +176,42 @@ export default function (props) {
             <label>Full Name</label>
             <input
               type="text"
+              id="name"
               className="form-control mt-1"
               placeholder="e.g Tanisha Kindo"
+              name="name"
+              autoComplete="off"
+              value={user.name}
+              onChange={handleinput}
             />
           </div>
-          <div className="form-group mt-3">
-            <label>Username</label>
-            <input
-              type="text"
-              className="form-control mt-1"
-              placeholder="e.g tanishakindo"
-            />
-          </div>
+        
           <div className="form-group mt-3">
             <label>Email ID</label>
             <input
               type="email"
+              id="email"
               className="form-control mt-1"
               placeholder="Email Address"
+              name="email"
+              value={user.email}
+              onChange={handleinput}
             />
           </div>
           <div className="form-group mt-3">
             <label>Create Password</label>
             <input
               type="password"
+              id="password"
               className="form-control mt-1"
               placeholder="Password"
+              name="password"
+              value={user.password}
+              onChange={handleinput}
             />
           </div>
           <div className="login-button d-grid gap-2 mt-3 mb-3">
-              <button type="button" class="btn btn-primary active" data-bs-toggle="button" aria-pressed="true">
+              <button type="button" class="btn btn-primary active" data-bs-toggle="button" aria-pressed="true" onClick={postData}>
                 SIGN UP
                 </button>
             </div>
@@ -132,4 +229,5 @@ export default function (props) {
       </>
   )
 }
+
 
